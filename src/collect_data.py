19f2e1ebca1756fe1439e8a4b0f582ebad139fef
lthurr <lthurr@gmail.com>
2014-11-25 21:53:56 -0300

    Updating the script to avoid followers with < 200 tweets.

--- src/collect_data.py
+++ src/collect_data.py
@@ -2,14 +2,16 @@ from twitter.connection import TwitterClient
 
 def main():
     tw_client = TwitterClient()
-    users = ['AgenciaTelam', 'LAVOZcomar', 'clarincom', 'C5N']
+    users = ['aprendengrande']
     print "Collecting data from Twitter"
+    user_id = 133
     for user in users:
         last_followers = tw_client.get_last_200_followers(user)
-        user_id = 0
-        for follower in last_followers['users']:
+        greater_than_200_tw_followers = [follower['screen_name'] for follower in last_followers['users'] if follower['statuses_count']>=200]
+        print len(greater_than_200_tw_followers)
+        for follower in greater_than_200_tw_followers:
             result_st = ""
-            screen_name = follower['screen_name']
+            screen_name = follower
             result_st += "[%s]\n" % screen_name
             print "Collecting data for the user %s" % screen_name
             last_tweets = tw_client.get_last_200_tweets(screen_name)
@@ -19,7 +21,7 @@ def main():
                 try:
                     result_st += screen_name + '_tweet_' + str(i) + '="%s"\n' % tweet['text'].replace('\\n', '<br>')
                 except Exception as e:
-                    print tweet['text']
+                    print tweet
                 i += 1
             with open('DATASET_%d.cfg' % user_id, 'wb') as configfile:
                 configfile.write(result_st.encode('utf8'))
